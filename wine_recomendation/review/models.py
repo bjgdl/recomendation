244ae537b2d101e6dea4b4007f035da249bc482d
Gaudel <codingcampworkspace@gmail.com>
2019-01-13 22:13:18 +0800

    model created

--- wine_recomendation/review/models.py
+++ wine_recomendation/review/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import numpy as np 
 # Create your models here.
+
+class Wine(models.Model):
+    name = models.CharField(max_length=200)
+
+    # def average_rating(self):
+    #     all_ratings = map(lambda x: x.rating, self.review_set.all() )
+    #     return np.mean(all_ratings)
+    
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+
+    RATING_CHOICES = (
+            (1, '1'),
+            (2,'2'),
+            (3,'3'),
+            (4,'4'),
+            (5,'5'),
+    )
+
+    wine = models.ForeignKey(Wine)
+    pub_date = models.DateTimeField('date_published')
+    user_name = models.CharField(max_length=20)
+    comment = models.CharField(max_length=5000)
+
+class Cluster(models.Model):
+    name = models.CharField(max_length=20)
+    user = models.ManyToManyField(User)
+
+    # def get_members(self):
+    #     return 'n'.join([u.username for u in self.users.all()])
